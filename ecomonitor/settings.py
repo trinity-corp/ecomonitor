62c9e20a969a539c344ae360ca450c6fe3668a51
Galukosi <61122724+galukosi@users.noreply.github.com>
2026-03-13 21:59:58 +0200

    Switch database configuration to PostgreSQL

--- ecomonitor/settings.py
+++ ecomonitor/settings.py
@@ -78,23 +78,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ecomonitor.wsgi.application'
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": env("DB_NAME"),
-#         "USER": env("DB_USER"),
-#         "PASSWORD": env("DB_PASSWORD"),
-#         "HOST": env("DB_HOST"),
-#         "PORT": env("DB_PORT"),
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+     "default": {
+        "ENGINE": "django.db.backends.postgresql",
+         "NAME": env("DB_NAME"),
+         "USER": env("DB_USER"),
+         "PASSWORD": env("DB_PASSWORD"),
+         "HOST": env("DB_HOST"),
+         "PORT": env("DB_PORT"),
+     }
 }
+
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
