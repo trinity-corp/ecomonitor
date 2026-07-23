482a0cc5c6628991db11f27580f8aad822bde5cc
galukosi <jker1786@gmail.com>
2026-07-23 16:40:33 +0300

    Make allowed hosts softcoded using environment variables

--- ecomonitor/settings.py
+++ ecomonitor/settings.py
@@ -25,7 +25,7 @@ SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = env("SECRET_KEY")
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ['ecomonitor-znv9.onrender.com']
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 INSTALLED_APPS = [
     'django.contrib.admin',
